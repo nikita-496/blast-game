995bf17a1a1af13173650b17f80d8b4877fb7b59
Nikita Shevkunov <shevkunovnikita97@gmail.com>
2022-01-27 12:35:53 +0500

    Add storage for game resources

---
+++ app/storage.js
@@ -0,0 +1,9 @@
+let path = {};
+
+//header
+path.LEVEL_FRAME = './assets/level.png';
+path.HIGHT_LIGHT_DECORATION = './assets/highlight.png';
+
+path.BOARD_IMAGE = './src/image/game-field.png';
+
+export default path;
