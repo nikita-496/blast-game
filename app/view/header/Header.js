3fdb4331b54ff112d7988df36f596ec5df709c79
Nikita Shevkunov <shevkunovnikita97@gmail.com>
2022-01-28 16:25:14 +0500

    Add haedaer layout game

--- app/view/header/Header.js
+++ app/view/header/Header.js
@@ -1,21 +1,100 @@
 import path from '../../storage';
 
-import Level from './level/Level';
-
+import CounterWithTitle from '../../customClasses/gameElement/counter/CounterWithTitle';
+import ProgressBar from '../../customClasses/gameElement/progressBar/ProgressBar';
 class Header extends cc.Layer {
   constructor() {
     super();
-    this._levelLabel = new Level('label', undefined, {
-      content: 'Уровень',
-      fontFamaly: 'Arial',
-      fontSize: 60,
-    });
-    this._spriteLabael = new Level('sprite', path.LEVEL_FRAME, undefined);
+    this._level = new CounterWithTitle();
+    this._progressBar = new ProgressBar();
+    this._amountScore = new CounterWithTitle();
     this.init();
   }
   init() {
-    this.addChild(this._levelLabel._element);
-    this.addChild(this._spriteLabael._element);
+    this.addChild(
+      this._level.createStaticLabel('Уровень', 'Arial', 40, { posWidth: 8, posHeight: 1.05 }, 0.5)
+    );
+    this.addChild(
+      this._level.createStaticSprite(path.LEVEL_FRAME, { posWidth: 8, posHeight: 1.15 }, 0.4)
+    );
+    this.addChild(
+      this._level.createStaticSprite(path.CIRCLE_GRAY, { posWidth: 10.9, posHeight: 1.149 }, 0.36),
+      1
+    );
+    this.addChild(
+      this._level.createStaticSprite(
+        path.HIGHT_LIGHT_DECORATION,
+        { posWidth: 6, posHeight: 1.13 },
+        0.36
+      )
+    );
+    this.addChild(
+      this._level.createDynamicLabel('2', 'Arial', 20, { posWidth: 8, posHeight: 1.15 }, 0.5),
+      1
+    );
+
+    //progress
+    this.addChild(this._progressBar._frame);
+    this.addChild(this._progressBar._title);
+    this.addChild(this._progressBar._progressScale);
+    this.addChild(this._progressBar._progressLine);
+
+    //amount
+    this.addChild(
+      this._amountScore.createDynamicLabel(
+        'Всего очков',
+        'Arial',
+        40,
+        { posWidth: 1.4, posHeight: 1.05 },
+        0.5
+      )
+    );
+    this.addChild(
+      this._amountScore.createStaticSprite(
+        path.LEVEL_FRAME,
+        { posWidth: 1.4, posHeight: 1.15 },
+        0.4
+      )
+    );
+    this.addChild(
+      this._amountScore.createStaticSprite(
+        path.CIRCLE_GRAY,
+        { posWidth: 1.465, posHeight: 1.148 },
+        0.36
+      ),
+      1
+    );
+    this.addChild(
+      this._amountScore.createStaticSprite(
+        path.HIGHT_LIGHT_DECORATION,
+        { posWidth: 1.32, posHeight: 1.13 },
+        0.36
+      ),
+      1
+    );
+    this.addChild(
+      this._amountScore.createDynamicLabel(
+        '6478',
+        'Arial',
+        20,
+        { posWidth: 1.37, posHeight: 1.15 },
+        0.5
+      ),
+      1
+    );
+    this.addChild(
+      this._amountScore.createStaticSprite(
+        path.PLUS_FRAME,
+        { posWidth: 1.25, posHeight: 1.15 },
+        0.45
+      )
+    );
+    this.addChild(
+      this._amountScore.createStaticSprite(path.PLUS, { posWidth: 1.25, posHeight: 1.15 }, 0.45),
+      1
+    );
+
+    //button
   }
 }
 
