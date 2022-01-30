3667079e840ad4e5f3ac3959200edb7bd15818e7
Nikita Shevkunov <shevkunovnikita97@gmail.com>
2022-01-30 15:29:20 +0500

    Add variable manager for game logic

---
+++ app/manager.js
@@ -0,0 +1,13 @@
+let value = {};
+
+value.xAxisStart = 389;
+value.yAxisTop = 510;
+value.xОffset = 52;
+value.yOffset = 55;
+
+module.exports.value = value;
+
+/*
+const xОffset = 52;
+  const yOffset = 55;
+*/
