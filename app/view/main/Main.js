a20769a0c2fa5bfe9587a224a64ec156f500182f
Nikita Shevkunov <shevkunovnikita97@gmail.com>
2022-01-31 14:14:48 +0500

    Implement event handling on a click on a tile

--- app/view/main/Main.js
+++ app/view/main/Main.js
@@ -6,15 +6,33 @@ import DynamicSprite from '../../interface/DynamicSprite';
 import staticSprite from '../../interface/staticSprite';
 import GameField from '../gameElement/gameField/GameField';
 import Tile from '../gameElement/tiles/Tile';
+import HandlerArea from '../../gameLogic/HandlerArea';
+import switchAdjusent from '../../gameLogic/switchAdjusent';
 
 class Main extends cc.Layer {
+  _sizeField = 9;
+  _hash = {};
+  _activeTile = null;
+  _test = null;
+
+  //hashSameTyepTiles
+  _area = null;
+  _testedTiles = null;
+  _counter = 0;
   constructor() {
     super();
     this._gameField = new GameField(new staticSprite());
-    this._tiles = Main.getTiles(9, 4);
+    this._tiles = Main.getTiles(this._sizeField, 4);
+    this.createListeners();
+
+    /*var size = cc.winSize;
+    this.sprite = new cc.Sprite(path.PURPLE_TILE);
+    this.sprite.attr({ x: size.width / 1.22, y: size.height / 2 });
+    this.addChild(this.sprite, 0);*/
 
     this.init();
   }
+
   init() {
     this.addChild(this._gameField._field);
 
@@ -23,13 +41,59 @@ class Main extends cc.Layer {
     }
   }
 
+  createListeners() {
+    const self = this;
+    const listener = cc.EventListener.create({
+      event: cc.EventListener.TOUCH_ONE_BY_ONE,
+      swallowTouches: true,
+      onTouchBegan: function (touch, event) {
+        const tap = touch.getLocation();
+
+        if (self._counter === 0) {
+          for (let i = 0; i < self._tiles.length; i++) {
+            const currTile = self._tiles[i];
+            const tileRect = currTile.getBoundingBox();
+            if (cc.rectContainsPoint(tileRect, tap)) {
+              const mainActive = new HandlerArea(currTile, self._tiles, self._testedTiles);
+
+              self._area = mainActive.fillHashTiles();
+
+              self._testedTiles = mainActive.setTestedTile();
+
+              self._counter = self._counter + 1;
+
+              break;
+            }
+          }
+        }
+
+        while (self._counter < Object.keys(self._area).length) {
+          const adjust = new HandlerArea(
+            switchAdjusent(self._testedTiles, self._area),
+            self._tiles,
+            self._testedTiles
+          );
+          self._area = { ...adjust.fillHashTiles(), ...self._area };
+          self._testedTiles = { ...adjust.setTestedTile(), ...self._testedTiles };
+
+          self._counter = self._counter + 1;
+        }
+
+        console.log(self._area);
+
+        return false;
+      },
+    });
+    cc.eventManager.addListener(listener, this);
+  }
+
   static getTiles(sizeRowGameField, numOfColors) {
     let tiles = [];
-    let tile = [];
+    let tile = {};
     for (let i = 1; i <= sizeRowGameField; i++) {
       const numRow = i;
       for (let k = 1; k <= sizeRowGameField; k++) {
-        const x = 805;
+        const x = 389;
         const y = Main.createRow(numRow);
         const adressColumnByX = Main.createColumn(x, sizeRowGameField);
 
